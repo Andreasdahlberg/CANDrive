61ec1c51e857371d494e724bb3be94fd2a20d5c8
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2021-05-05 23:09:10 +0200

    Add blink example using libopencm3

--- firmware/app/main/main.c
+++ firmware/app/main/main.c
@@ -1,9 +1,21 @@
-int main()
-{
-    while (1)
-    {
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+int main(void) {
+    rcc_periph_clock_enable(RCC_GPIOA);
+
+    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO5);
+
+    gpio_set(GPIOA, GPIO5);
+
+
+    while (1) {
+        /* wait a little bit */
+        for (int i = 0; i < 200000; i++) {
+            __asm__("nop");
+        }
+        gpio_toggle(GPIOA, GPIO5);
 
     }
-
-    return 0;
 }
