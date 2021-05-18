e79e9be324942bdfe691b96c39c9aaee51707a10
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2021-05-18 09:06:37 +0200

    Add usart setup

--- firmware/app/main/main.c
+++ firmware/app/main/main.c
@@ -1,21 +1,99 @@
 
+/**
+ * @file   main.c
+ * @Author Andreas Dahlberg (andreas.dahlberg90@gmail.com)
+ * @brief  Main application
+ */
+
+/*
+This file is part of CANDrive firmware.
+
+CANDrive firmware is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CANDrive firmware is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//////////////////////////////////////////////////////////////////////////
+//INCLUDES
+//////////////////////////////////////////////////////////////////////////
+
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/cm3/nvic.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(void) {
+static void clock_setup(void)
+{
+    rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+
+    /* Enable GPIOA clock (for LED GPIOs). */
+    rcc_periph_clock_enable(RCC_GPIOC);
+
+    /* Enable clocks for GPIO port A (for GPIO_USART2_TX) and USART2. */
     rcc_periph_clock_enable(RCC_GPIOA);
+    rcc_periph_clock_enable(RCC_AFIO);
+    rcc_periph_clock_enable(RCC_USART2);
+}
 
-    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO5);
+static void usart_setup(void)
+{
+    /* Setup GPIO pin GPIO_USART2_RE_TX on GPIO port B for transmit. */
+    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
+                  GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART2_TX);
 
+    /* Setup UART parameters. */
+    usart_set_baudrate(USART2, 115200);
+    usart_set_databits(USART2, 8);
+    usart_set_stopbits(USART2, USART_STOPBITS_1);
+    usart_set_parity(USART2, USART_PARITY_NONE);
+    usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+    usart_set_mode(USART2, USART_MODE_TX);
+
+    /* Finally enable the USART. */
+    usart_enable(USART2);
+}
+
+static void gpio_setup(void)
+{
     gpio_set(GPIOA, GPIO5);
 
+    /* Setup GPIO6 and 7 (in GPIO port A) for LED use. */
+    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
+                  GPIO_CNF_OUTPUT_PUSHPULL, GPIO5);
+}
 
+//////////////////////////////////////////////////////////////////////////
+//FUNCTIONS
+//////////////////////////////////////////////////////////////////////////
+
+int main(void)
+{
+    clock_setup();
+    gpio_setup();
+    usart_setup();
+
+    uint32_t a = 0;
     while (1) {
-        /* wait a little bit */
-        for (int i = 0; i < 200000; i++) {
+        gpio_toggle(GPIOA, GPIO5);
+        printf("Hello %lu!\r\n", a);
+        ++a;
+
+        for (size_t i = 0; i < 800000; i++)    /* Wait a bit. */
+        {
             __asm__("nop");
         }
-        gpio_toggle(GPIOA, GPIO5);
-
     }
+
+    return 0;
 }
