305958aa464dcaa5d347e1b5e34d9a456aae0c60
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2021-06-29 14:39:07 +0200

    Add more unit tests for the motor module

--- firmware/app/motor/test/test_motor.c
+++ firmware/app/motor/test/test_motor.c
@@ -32,7 +32,8 @@ along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
 #include <cmocka.h>
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <stdlib.h>
+#include "utility.h"
 #include "motor.h"
 #include "pwm.h"
 
@@ -51,6 +52,7 @@ along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
 
 static struct logging_logger_t *dummy_logger;
 const uint32_t DEFAULT_PWM_FREQUENCY = 20000;
+struct motor_t motor;
 
 //////////////////////////////////////////////////////////////////////////
 //LOCAL FUNCTIONS
@@ -58,7 +60,17 @@ const uint32_t DEFAULT_PWM_FREQUENCY = 20000;
 
 static int Setup(void **state)
 {
+    const char motor_name[] = "TM1";
+    struct pwm_output_t pwm;
+
+    motor = (__typeof__(motor)) {0};
+
     will_return(Logging_GetLogger, dummy_logger);
+    expect_function_call(PWM_Disable);
+    expect_value(PWM_SetFrequency, frequency, DEFAULT_PWM_FREQUENCY);
+    expect_value(PWM_SetDuty, duty, 0);
+
+    Motor_Init(&motor, motor_name, &pwm);
 
     return 0;
 }
@@ -70,7 +82,6 @@ static int Setup(void **state)
 static void test_Motor_Init_Error(void **state)
 {
     const char motor_name[] = "TM1";
-    struct motor_t motor;
     struct pwm_output_t pwm;
 
     expect_assert_failure(Motor_Init(NULL, motor_name, &pwm));
@@ -81,7 +92,6 @@ static void test_Motor_Init_Error(void **state)
 static void test_Motor_Init(void **state)
 {
     const char motor_name[] = "TM1";
-    struct motor_t motor;
     struct pwm_output_t pwm;
 
     will_return(Logging_GetLogger, dummy_logger);
@@ -90,6 +100,107 @@ static void test_Motor_Init(void **state)
     expect_value(PWM_SetDuty, duty, 0);
 
     Motor_Init(&motor, motor_name, &pwm);
+}
+
+static void test_Motor_GetRPM_Invalid(void **state)
+{
+    expect_assert_failure(Motor_GetRPM(NULL));
+}
+
+static void test_Motor_GetRPM(void **state)
+{
+    assert_int_equal(Motor_GetRPM(&motor), 0);
+}
+
+static void test_Motor_GetCurrent_Invalid(void **state)
+{
+    expect_assert_failure(Motor_GetCurrent(NULL));
+}
+
+static void test_Motor_GetCurrent(void **state)
+{
+    assert_int_equal(Motor_GetCurrent(&motor), 0);
+}
+
+static void test_Motor_SetSpeed_Invalid(void **state)
+{
+    expect_assert_failure(Motor_SetSpeed(NULL, 0));
+
+    int16_t invalid_speeds[] = {INT16_MIN, -101, 101, INT16_MAX};
+    for (size_t i = 0; i < ElementsIn(invalid_speeds); ++i)
+    {
+        expect_assert_failure(Motor_SetSpeed(&motor, invalid_speeds[i]));
+    }
+}
+
+static void test_Motor_SetSpeed(void **state)
+{
+    int16_t speeds[] = {-100, -25, 0, 50, 100};
+    for (size_t i = 0; i < ElementsIn(speeds); ++i)
+    {
+        const int16_t speed = speeds[i];
+
+        expect_function_call(PWM_Disable);
+        expect_value(PWM_SetDuty, duty, abs(speed));
+        expect_function_call(PWM_Enable);
+
+        Motor_SetSpeed(&motor, speed);
+    }
+}
+
+static void test_Motor_Coast_Invalid(void **state)
+{
+    expect_assert_failure(Motor_Coast(NULL));
+}
+
+static void test_Motor_Coast(void **state)
+{
+    expect_value(PWM_SetDuty, duty, 0);
+    Motor_Coast(&motor);
+}
+
+static void test_Motor_Run(void **state)
+{
+    const int16_t speed = 75;
+
+    /* Set speed */
+    expect_function_call(PWM_Disable);
+    expect_value(PWM_SetDuty, duty, speed);
+    expect_function_call(PWM_Enable);
+    Motor_SetSpeed(&motor, speed);
+
+    /* Coast */
+    expect_value(PWM_SetDuty, duty, 0);
+    Motor_Coast(&motor);
+
+    /*Run motor again and expect the set speed again. */
+    expect_function_call(PWM_Disable);
+    expect_value(PWM_SetDuty, duty, speed);
+    expect_function_call(PWM_Enable);
+    Motor_Run(&motor);
+}
+
+static void test_Motor_Brake_Invalid(void **state)
+{
+    expect_assert_failure(Motor_Brake(NULL));
+}
+
+static void test_Motor_Brake(void **state)
+{
+    expect_function_call(PWM_Disable);
+    expect_value(PWM_SetDuty, duty, 100);
+    expect_function_call(PWM_Enable);
+    Motor_Brake(&motor);
+}
+
+static void test_Motor_GetStatus_Invalid(void **state)
+{
+    expect_assert_failure(Motor_GetStatus(NULL));
+}
+
+static void test_Motor_GetStatus(void **state)
+{
+    assert_int_equal(Motor_GetStatus(&motor), MOTOR_UNKNOWN);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -101,7 +212,20 @@ int main(int argc, char *argv[])
     const struct CMUnitTest test_FIFO[] =
     {
         cmocka_unit_test(test_Motor_Init_Error),
-        cmocka_unit_test(test_Motor_Init)
+        cmocka_unit_test(test_Motor_Init),
+        cmocka_unit_test_setup(test_Motor_GetRPM_Invalid, Setup),
+        cmocka_unit_test_setup(test_Motor_GetRPM, Setup),
+        cmocka_unit_test_setup(test_Motor_GetCurrent_Invalid, Setup),
+        cmocka_unit_test_setup(test_Motor_GetCurrent, Setup),
+        cmocka_unit_test_setup(test_Motor_SetSpeed_Invalid, Setup),
+        cmocka_unit_test_setup(test_Motor_SetSpeed, Setup),
+        cmocka_unit_test_setup(test_Motor_Coast_Invalid, Setup),
+        cmocka_unit_test_setup(test_Motor_Coast, Setup),
+        cmocka_unit_test_setup(test_Motor_Run, Setup),
+        cmocka_unit_test_setup(test_Motor_Brake_Invalid, Setup),
+        cmocka_unit_test_setup(test_Motor_Brake, Setup),
+        cmocka_unit_test_setup(test_Motor_GetStatus_Invalid, Setup),
+        cmocka_unit_test_setup(test_Motor_GetStatus, Setup),
     };
 
     if (argc >= 2)
