8d07847c5b1bb5bb8565dcba3ad775524566cfc4
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2021-09-10 23:45:40 +0200

    Add board mock

---
+++ firmware/app/board/test/mock/mock_board.c
@@ -0,0 +1,82 @@
+/**
+ * @file   mock_board.c
+ * @Author Andreas Dahlberg
+ * @brief  Mock functions for board.
+ */
+
+/*
+This file is part of CANDrive firmware.
+
+CANDrive firmware is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CANDrive firmware is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//////////////////////////////////////////////////////////////////////////
+//INCLUDES
+//////////////////////////////////////////////////////////////////////////
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include <stdio.h>
+#include "board.h"
+
+//////////////////////////////////////////////////////////////////////////
+//DEFINES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//TYPE DEFINITIONS
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//VARIABLES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//LOCAL FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//FUNCTIONS
+//////////////////////////////////////////////////////////////////////////
+
+__attribute__((weak)) void Board_Init(void)
+{
+}
+
+__attribute__((weak)) uint32_t Board_GetHardwareRevision(void)
+{
+    return mock_type(uint32_t);
+}
+
+__attribute__((weak)) uint32_t Board_GetSoftwareRevision(void)
+{
+    return mock_type(uint32_t);
+}
+
+__attribute__((weak)) const struct motor_config_t *Board_GetMotorConfig(size_t index)
+{
+    return mock_type(struct motor_config_t *);
+}
+
+__attribute__((weak)) size_t Board_GetNumberOfMotors(void)
+{
+    return mock_type(size_t);
+}
+
+//////////////////////////////////////////////////////////////////////////
+//LOCAL FUNCTIONS
+//////////////////////////////////////////////////////////////////////////
