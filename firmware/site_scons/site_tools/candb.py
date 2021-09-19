76285f088f6e9ae21b79bc80d3eca1be1bf10011
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2021-09-19 23:20:35 +0200

    Fix issue with candb dependencies

--- firmware/site_scons/site_tools/candb.py
+++ firmware/site_scons/site_tools/candb.py
@@ -17,27 +17,40 @@
 
 import SCons.Util
 import SCons.Tool.cc as cc
+from SCons.Script import Dir
+import os
 
 __author__ = 'andreas.dahlberg90@gmail.com (Andreas Dahlberg)'
+
+CANTOOLS = 'cantools'
+
+def candb_emitter(target, source, env):
+    path_of_calling_sconscript = Dir('.').srcnode().path
+    module_name = os.path.basename(path_of_calling_sconscript)
+    target.extend([module_name + '.c', module_name + '.h'])
+    return target, source
+
 
 def generate(env):
     """Add Builders and construction variables for the compiler to an Environment."""
     cc.generate(env)
 
+    env['CANTOOLS'] = env.Detect(CANTOOLS)
+    env['CANTOOLSCOM'] = '$CANTOOLS generate_c_source --no-floating-point-numbers --database-name ${TARGET.filebase} --output-directory ${TARGET.dir} ${SOURCE}'
+    env['CANTOOLSSRCSUFFIX']  = '.dbc'
     env.Append(BUILDERS={
         'CANDB': _get_candb_builder()
     })
 
 
 def exists(env):
-
-    return True
+    return env.Detect('CANTOOLS')
 
 
 def _get_candb_builder():
 
     return SCons.Builder.Builder(
-        action=SCons.Action.Action(
-            'cantools generate_c_source --no-floating-point-numbers --database-name ${TARGET.filebase} --output-directory ${TARGET.dir} ${SOURCE}'
-        )
+        action=SCons.Action.Action('$CANTOOLSCOM'),
+        emitter=candb_emitter,
+        srcsuffix = '$CANTOOLSSRCSUFFIX'
     )
