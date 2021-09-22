f01c69c5e34722b9e6554583e4ccba17aadf1cfb
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2021-09-22 12:19:23 +0200

    Add new mocks

---
+++ firmware/app/can_interface/test/mock/mock_can_interface.c
@@ -0,0 +1,81 @@
+/**
+ * @file   mock_can_interface.c
+ * @Author Andreas Dahlberg
+ * @brief  Mock functions for can_interface.
+ */
+
+/*
+This file is part of CANDrive firmware.
+
+CANDrive firmware is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CANDrive firmware is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//////////////////////////////////////////////////////////////////////////
+//INCLUDES
+//////////////////////////////////////////////////////////////////////////
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include <stdio.h>
+#include "can_interface.h"
+
+//////////////////////////////////////////////////////////////////////////
+//DEFINES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//TYPE DEFINITIONS
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//VARIABLES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//LOCAL FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//FUNCTIONS
+//////////////////////////////////////////////////////////////////////////
+
+__attribute__((weak)) void CANInterface_Init(void)
+{
+    function_called();
+}
+
+__attribute__((weak)) bool CANInterface_Transmit(uint32_t id, void *data_p, size_t size)
+{
+    assert_non_null(data_p);
+    mock_type(bool);
+}
+
+__attribute__((weak)) void CANInterface_RegisterListener(caninterface_listener_cb_t listener_cb)
+{
+    check_expected(listener_cb);
+}
+
+__attribute__((weak)) void CANInterface_AddFilter(uint16_t id, uint16_t mask)
+{
+    check_expected(id);
+    check_expected(mask);
+}
+
+//////////////////////////////////////////////////////////////////////////
+//LOCAL FUNCTIONS
+//////////////////////////////////////////////////////////////////////////
---
+++ firmware/app/motor_controller/test/mock/mock_motor_controller_cmd.c
@@ -0,0 +1,85 @@
+/**
+ * @file   mock_motor_controller_cmd.c
+ * @Author Andreas Dahlberg
+ * @brief  Mock functions for motor_controller_cmd.
+ */
+
+/*
+This file is part of CANDrive firmware.
+
+CANDrive firmware is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CANDrive firmware is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//////////////////////////////////////////////////////////////////////////
+//INCLUDES
+//////////////////////////////////////////////////////////////////////////
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include <stdio.h>
+#include "motor_controller_cmd.h"
+
+//////////////////////////////////////////////////////////////////////////
+//DEFINES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//TYPE DEFINITIONS
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//VARIABLES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//LOCAL FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//FUNCTIONS
+//////////////////////////////////////////////////////////////////////////
+
+__attribute__((weak)) bool MotorControllerCmd_SetRPM(void)
+{
+    mock_type(bool);
+}
+
+__attribute__((weak)) bool MotorControllerCmd_SetCurrent(void)
+{
+    mock_type(bool);
+}
+
+__attribute__((weak)) bool MotorControllerCmd_Run(void)
+{
+    mock_type(bool);
+}
+
+__attribute__((weak)) bool MotorControllerCmd_Coast(void)
+{
+    mock_type(bool);
+}
+
+__attribute__((weak)) bool MotorControllerCmd_Brake(void)
+{
+    mock_type(bool);
+}
+
+//////////////////////////////////////////////////////////////////////////
+//LOCAL FUNCTIONS
+//////////////////////////////////////////////////////////////////////////
+
