862f16e6f84bb458cb8fe02e6038174d22449cb3
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2021-09-24 12:53:24 +0200

    Add fallback mirror to cmocka installation script

--- scripts/install_cmocka.sh
+++ scripts/install_cmocka.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-#Get source
-git clone https://git.cryptomilk.org/projects/cmocka.git
+#Get source, use gitlab mirror as fallback.
+if !(git clone https://git.cryptomilk.org/projects/cmocka.git) then
+    echo "Clone from gitlab mirror..."
+    if !(git clone https://gitlab.com/cmocka/cmocka.git) then
+        exit 1
+    fi
+fi
 
 #Compile and install
 mkdir cmocka/build
