1dac2f859a82263a71898c02947e4e8c5c8cefb3
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2021-11-16 22:54:17 +0100

    Add NVS unit test skeleton

---
+++ firmware/app/nvs/test/mock/mock_nvs.c
@@ -0,0 +1,81 @@
+/**
+ * @file   mock_nvs.c
+ * @Author Andreas Dahlberg
+ * @brief  Mock functions for the NVS module.
+ */
+
+/*
+This file is part of CANDrive firmware.
+
+CANDrive firmware is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CANDrive firmware is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//////////////////////////////////////////////////////////////////////////
+//INCLUDES
+//////////////////////////////////////////////////////////////////////////
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include <stdio.h>
+#include "nvs.h"
+
+//////////////////////////////////////////////////////////////////////////
+//DEFINES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//TYPE DEFINITIONS
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//VARIABLES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//LOCAL FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//FUNCTIONS
+//////////////////////////////////////////////////////////////////////////
+
+__attribute__((weak)) void NVS_Init(uint32_t start_page_address, size_t number_of_pages)
+{
+    function_called();
+}
+__attribute__((weak)) bool NVS_Retrieve(const char *key_p, uint32_t *value_p)
+{
+    assert_non_null(key_p);
+    assert_non_null(value_p);
+
+    *value_p = mock_type(uint32_t);
+    return mock_type(bool);
+}
+__attribute__((weak)) bool NVS_Store(const char *key_p, uint32_t value)
+{
+    assert_non_null(key_p);
+
+    return mock_type(bool);
+}
+__attribute__((weak)) bool NVS_Clear(void)
+{
+    return mock_type(bool);
+}
+
+//////////////////////////////////////////////////////////////////////////
+//LOCAL FUNCTIONS
+//////////////////////////////////////////////////////////////////////////
