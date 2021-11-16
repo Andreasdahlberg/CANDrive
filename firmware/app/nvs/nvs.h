18e91aabfe7db12b6740b6e24b7962e90f7847ae
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2021-11-16 23:04:36 +0100

    More work on NVS module

--- firmware/app/nvs/nvs.h
+++ firmware/app/nvs/nvs.h
@@ -1,7 +1,7 @@
 /**
  * @file   nvs.h
  * @Author Andreas Dahlberg (andreas.dahlberg90@gmail.com)
- * @brief  Non volatile storage module.
+ * @brief  Non Volatile Storage module.
  */
 
 /*
@@ -40,12 +40,38 @@ along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
 //TYPE DEFINITIONS
 //////////////////////////////////////////////////////////////////////////
 
+/**
+ * Initialize the Non Volatile Storage(NVS) module.
+ *
+ * @param start_page_address Address of the first flash page used for NVS.
+ * @param number_of_pages Number of flash pages used for NVS.
+ */
 void NVS_Init(uint32_t start_page_address, size_t number_of_pages);
 
-bool NVS_Retrieve(const char *key_p, uint32_t *value_p);
-
+/**
+ * Store a value in NVS.
+ *
+ * @param  key_p Key of value to store.
+ * @param  value Value to store.
+ * @return True if value was stored successfully, otherwise false.
+ */
 bool NVS_Store(const char *key_p, uint32_t value);
 
+/**
+ * Retrieve a value from NVS.
+ *
+ * @param  key_p Key of value to retrieve.
+ * @param  value_p Pointer to the retrieved value.
+ *
+ * @return True if value for the supplied key existed, otherwise false.
+ */
+bool NVS_Retrieve(const char *key_p, uint32_t *value_p);
+
+/**
+ * Clear all stored values.
+ *
+ * @return Status of clear operation.
+ */
 bool NVS_Clear(void);
 
 #endif
