ede39a9ef5e2d0a36e081e5b8185363f065dacef
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2021-11-23 12:23:44 +0100

    Add unit tests for NVS module

--- firmware/app/nvs/test/test_nvs.c
+++ firmware/app/nvs/test/test_nvs.c
@@ -32,11 +32,20 @@ along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
 #include <cmocka.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <libopencm3/stm32/flash.h>
 #include "nvs.h"
 
 //////////////////////////////////////////////////////////////////////////
 //DEFINES
 //////////////////////////////////////////////////////////////////////////
+
+/**
+ * Since uint32_t is used for addresses in the target code, a virtual 32-bit
+ * address is sent to the tested code and then translated to a 64-bit address
+ * in the flash mock.
+ */
+#define FLASH_START_ADDRESS 0
+#define NUMBER_OF_PAGES 2
 
 //////////////////////////////////////////////////////////////////////////
 //TYPE DEFINITIONS
@@ -46,20 +55,161 @@ along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
 //VARIABLES
 //////////////////////////////////////////////////////////////////////////
 
+static struct logging_logger_t *dummy_logger;
+static bool create_corrupt_crc = false;
+
 //////////////////////////////////////////////////////////////////////////
 //LOCAL FUNCTIONS
 //////////////////////////////////////////////////////////////////////////
+
+static int Setup(void **state)
+{
+    create_corrupt_crc = false;
+
+    flash_erase_all_pages();
+    will_return_maybe(Logging_GetLogger, dummy_logger);
+    will_return_maybe(flash_get_status_flags, FLASH_SR_EOP);
+
+    NVS_Init(FLASH_START_ADDRESS, NUMBER_OF_PAGES);
+
+    return 0;
+}
 
 //////////////////////////////////////////////////////////////////////////
 //TESTS
 //////////////////////////////////////////////////////////////////////////
 
-static void test_NVS_Init(void **state)
+static void test_NVS_Init_Invalid(void **state)
 {
-    skip();
+    expect_assert_failure(NVS_Init(FLASH_START_ADDRESS, 0));
+    expect_assert_failure(NVS_Init(FLASH_START_ADDRESS, 1));
 }
 
-//////////////////////////////////////////////////////////////////////////
+static void test_NVS_StoreAndRetrieve(void **state)
+{
+    will_return_maybe(flash_get_status_flags, FLASH_SR_EOP);
+
+    assert_true(NVS_Store("Foo", 10));
+    assert_true(NVS_Store("Bar", 20));
+    assert_true(NVS_Store("Foo", 30));
+
+    uint32_t value;
+    assert_true(NVS_Retrieve("Foo", &value));
+    assert_int_equal(value, 30);
+    assert_true(NVS_Retrieve("Bar", &value));
+    assert_int_equal(value, 20);
+    assert_false(NVS_Retrieve("Foobar", &value));
+}
+
+static void test_NVS_RetrieveExistingValues(void **state)
+{
+    will_return_maybe(Logging_GetLogger, dummy_logger);
+    will_return_maybe(flash_get_status_flags, FLASH_SR_EOP);
+
+    assert_true(NVS_Store("Foo", 50));
+    assert_true(NVS_Store("Bar", 60));
+    assert_true(NVS_Store("Bar", 70));
+
+    NVS_Init(FLASH_START_ADDRESS, NUMBER_OF_PAGES);
+
+    uint32_t value;
+    assert_true(NVS_Retrieve("Foo", &value));
+    assert_int_equal(value, 50);
+    assert_true(NVS_Retrieve("Bar", &value));
+    assert_int_equal(value, 70);
+    assert_false(NVS_Retrieve("Foobar", &value));
+}
+
+static void test_NVS_PageFull(void **state)
+{
+    will_return_maybe(Logging_GetLogger, dummy_logger);
+    will_return_maybe(flash_get_status_flags, FLASH_SR_EOP);
+
+    /* Fill up the first page */
+    for (size_t i = 0; i < 16; ++i)
+    {
+        assert_true(NVS_Store("A", i));
+        assert_true(NVS_Store("B", i));
+        assert_true(NVS_Store("C", i));
+        assert_true(NVS_Store("D", i));
+    }
+
+    /* Verify that the correct values was moved to the new page. */
+    uint32_t value;
+    assert_true(NVS_Retrieve("A", &value));
+    assert_int_equal(value, 15);
+    assert_true(NVS_Retrieve("B", &value));
+    assert_int_equal(value, 15);
+    assert_true(NVS_Retrieve("C", &value));
+    assert_int_equal(value, 15);
+    assert_true(NVS_Retrieve("D", &value));
+    assert_int_equal(value, 15);
+
+    /* Verify that a new value is stored correctly on the new page. */
+    assert_true(NVS_Store("D", 16));
+    assert_true(NVS_Retrieve("D", &value));
+    assert_int_equal(value, 16);
+    assert_true(NVS_Retrieve("A", &value));
+    assert_int_equal(value, 15);
+
+    /* Verify that the correct page is used after init. */
+    NVS_Init(FLASH_START_ADDRESS, NUMBER_OF_PAGES);
+    assert_true(NVS_Retrieve("B", &value));
+    assert_int_equal(value, 15);
+    assert_true(NVS_Retrieve("D", &value));
+    assert_int_equal(value, 16);
+}
+
+static void test_NVS_PageWrapAround(void **state)
+{
+    will_return_maybe(Logging_GetLogger, dummy_logger);
+    will_return_maybe(flash_get_status_flags, FLASH_SR_EOP);
+
+    /* Fill up the first page. */
+    for (size_t i = 0; i < 16; ++i)
+    {
+        assert_true(NVS_Store("A", i));
+        assert_true(NVS_Store("B", i));
+        assert_true(NVS_Store("C", i));
+        assert_true(NVS_Store("D", i));
+    }
+
+    /* Fill up the second page. */
+    for (size_t i = 16; i < 32; ++i)
+    {
+        assert_true(NVS_Store("A", i));
+        assert_true(NVS_Store("B", i));
+        assert_true(NVS_Store("C", i));
+        assert_true(NVS_Store("D", i));
+    }
+
+    /* Verify that the correct values was moved to the new page. */
+    uint32_t value;
+    assert_true(NVS_Retrieve("A", &value));
+    assert_int_equal(value, 31);
+    assert_true(NVS_Retrieve("B", &value));
+    assert_int_equal(value, 31);
+    assert_true(NVS_Retrieve("C", &value));
+    assert_int_equal(value, 31);
+    assert_true(NVS_Retrieve("D", &value));
+    assert_int_equal(value, 31);
+
+    /*Verify that a new value is stored correctly on the new page. */
+    assert_true(NVS_Store("C", 32));
+    assert_true(NVS_Retrieve("C", &value));
+    assert_int_equal(value, 32);
+    assert_true(NVS_Retrieve("B", &value));
+    assert_int_equal(value, 31);
+
+    /* Verify that the correct page is used after init. */
+    NVS_Init(FLASH_START_ADDRESS, NUMBER_OF_PAGES);
+    assert_true(NVS_Retrieve("C", &value));
+    assert_int_equal(value, 32);
+    assert_true(NVS_Retrieve("A", &value));
+    assert_int_equal(value, 31);
+}
+
+/////////////////////////////////////////////////////////////////////////
 //FUNCTIONS
 //////////////////////////////////////////////////////////////////////////
 
@@ -67,7 +217,11 @@ int main(int argc, char *argv[])
 {
     const struct CMUnitTest test_nvs[] =
     {
-        cmocka_unit_test(test_NVS_Init)
+        cmocka_unit_test(test_NVS_Init_Invalid),
+        cmocka_unit_test_setup(test_NVS_StoreAndRetrieve, Setup),
+        cmocka_unit_test_setup(test_NVS_RetrieveExistingValues, Setup),
+        cmocka_unit_test_setup(test_NVS_PageFull, Setup),
+        cmocka_unit_test_setup(test_NVS_PageWrapAround, Setup),
     };
 
     if (argc >= 2)
@@ -76,4 +230,21 @@ int main(int argc, char *argv[])
     }
 
     return cmocka_run_group_tests(test_nvs, NULL, NULL);
+}
+
+uint32_t CRC_Calculate(const void *data_p, size_t length)
+{
+    uint32_t crc = 0xAAAAAAAA;
+
+    for(size_t i = 0; i < length; ++i)
+    {
+        crc += ((uint8_t *)data_p)[i];
+    }
+
+    if (create_corrupt_crc)
+    {
+        crc += 1;
+    }
+
+    return crc;
 }
--- firmware/app/test/mocks/mock_flash.c
+++ firmware/app/test/mocks/mock_flash.c
@@ -31,11 +31,15 @@ along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
 #include <setjmp.h>
 #include <cmocka.h>
 #include <stdio.h>
+#include <string.h>
 #include <libopencm3/stm32/flash.h>
 
 //////////////////////////////////////////////////////////////////////////
 //DEFINES
 //////////////////////////////////////////////////////////////////////////
+
+#define NUMBER_OF_PAGES 2
+#define PAGE_SIZE 1024
 
 //////////////////////////////////////////////////////////////////////////
 //TYPE DEFINITIONS
@@ -45,6 +49,8 @@ along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
 //VARIABLES
 //////////////////////////////////////////////////////////////////////////
 
+static uint32_t flash_data[NUMBER_OF_PAGES][PAGE_SIZE];
+
 //////////////////////////////////////////////////////////////////////////
 //LOCAL FUNCTION PROTOTYPES
 //////////////////////////////////////////////////////////////////////////
@@ -52,6 +58,14 @@ along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
 //////////////////////////////////////////////////////////////////////////
 //FUNCTIONS
 //////////////////////////////////////////////////////////////////////////
+
+void *__real_memcpy (void *destination_p, const void *source_p, size_t length);
+
+void *__wrap_memcpy (void *destination_p, const void *source_p, size_t length)
+{
+    uint8_t *real_source_p = ((uint8_t *)flash_data) + ((uintptr_t )source_p);
+    return __real_memcpy(destination_p, real_source_p, length);
+}
 
 __attribute__((weak)) void flash_halfcycle_enable(void)
 {
@@ -110,27 +124,30 @@ __attribute__((weak)) uint32_t flash_get_status_flags(void)
 
 __attribute__((weak)) void flash_clear_status_flags(void)
 {
-    function_called();
+
 }
 
 __attribute__((weak)) void flash_program_word(uint32_t address, uint32_t data)
 {
-    check_expected(address);
+    uint8_t *destination_p = ((uint8_t *)flash_data) + address;
+    __real_memcpy(destination_p, &data, sizeof(data));
 }
 
 __attribute__((weak)) void flash_program_half_word(uint32_t address, uint16_t data)
 {
-    check_expected(address);
+
 }
 
 __attribute__((weak)) void flash_erase_page(uint32_t page_address)
 {
-    check_expected(page_address);
+    uint32_t page_index = page_address / PAGE_SIZE;
+    memset(flash_data[page_index], 0xFF, PAGE_SIZE);
 }
 
 __attribute__((weak)) void flash_erase_all_pages(void)
 {
-    function_called();
+
+    memset(flash_data, 0xFF, sizeof(flash_data));
 }
 
 __attribute__((weak)) void flash_erase_option_bytes(void)
