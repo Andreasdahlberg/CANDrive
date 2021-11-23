8f201e2442e16ce2af529cd9d87f9f016a4d5788
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2021-11-23 12:55:14 +0100

    Fix build error

--- firmware/app/test/mocks/mock_flash.c
+++ firmware/app/test/mocks/mock_flash.c
@@ -31,15 +31,11 @@ along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
 #include <setjmp.h>
 #include <cmocka.h>
 #include <stdio.h>
-#include <string.h>
 #include <libopencm3/stm32/flash.h>
 
 //////////////////////////////////////////////////////////////////////////
 //DEFINES
 //////////////////////////////////////////////////////////////////////////
-
-#define NUMBER_OF_PAGES 2
-#define PAGE_SIZE 1024
 
 //////////////////////////////////////////////////////////////////////////
 //TYPE DEFINITIONS
@@ -49,8 +45,6 @@ along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
 //VARIABLES
 //////////////////////////////////////////////////////////////////////////
 
-static uint32_t flash_data[NUMBER_OF_PAGES][PAGE_SIZE];
-
 //////////////////////////////////////////////////////////////////////////
 //LOCAL FUNCTION PROTOTYPES
 //////////////////////////////////////////////////////////////////////////
@@ -58,14 +52,6 @@ static uint32_t flash_data[NUMBER_OF_PAGES][PAGE_SIZE];
 //////////////////////////////////////////////////////////////////////////
 //FUNCTIONS
 //////////////////////////////////////////////////////////////////////////
-
-void *__real_memcpy (void *destination_p, const void *source_p, size_t length);
-
-void *__wrap_memcpy (void *destination_p, const void *source_p, size_t length)
-{
-    uint8_t *real_source_p = ((uint8_t *)flash_data) + ((uintptr_t )source_p);
-    return __real_memcpy(destination_p, real_source_p, length);
-}
 
 __attribute__((weak)) void flash_halfcycle_enable(void)
 {
@@ -129,8 +115,7 @@ __attribute__((weak)) void flash_clear_status_flags(void)
 
 __attribute__((weak)) void flash_program_word(uint32_t address, uint32_t data)
 {
-    uint8_t *destination_p = ((uint8_t *)flash_data) + address;
-    __real_memcpy(destination_p, &data, sizeof(data));
+
 }
 
 __attribute__((weak)) void flash_program_half_word(uint32_t address, uint16_t data)
@@ -140,14 +125,12 @@ __attribute__((weak)) void flash_program_half_word(uint32_t address, uint16_t da
 
 __attribute__((weak)) void flash_erase_page(uint32_t page_address)
 {
-    uint32_t page_index = page_address / PAGE_SIZE;
-    memset(flash_data[page_index], 0xFF, PAGE_SIZE);
+
 }
 
 __attribute__((weak)) void flash_erase_all_pages(void)
 {
 
-    memset(flash_data, 0xFF, sizeof(flash_data));
 }
 
 __attribute__((weak)) void flash_erase_option_bytes(void)
