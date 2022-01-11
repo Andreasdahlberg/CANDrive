25923ea4a4bd7963ba031f1bf4dc90a41f092ae8
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2022-01-11 07:18:48 +0100

    Increas max number of listeners to CAN interface

--- firmware/app/can_interface/can_interface.c
+++ firmware/app/can_interface/can_interface.c
@@ -44,7 +44,7 @@ along with CANDrive firmware.  If not, see <http://www.gnu.org/licenses/>.
 #define CANIF_LOGGER_DEBUG_LEVEL LOGGING_INFO
 #endif
 
-#define MAX_NUMBER_OF_LISTENERS 1
+#define MAX_NUMBER_OF_LISTENERS 2
 
 /**
  * Since 16-bit filter scale is used, the max number of filters are twice the
