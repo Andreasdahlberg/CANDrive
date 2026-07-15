a65cb6db62af4955566bc4daa88714e7864244a8
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2026-07-15 22:55:52 +0200

    Update the Sonar scanner/build wrapper install script

--- scripts/install_sonarscanner_build_wrapper.sh
+++ scripts/install_sonarscanner_build_wrapper.sh
@@ -7,10 +7,10 @@ cp /usr/local/bin/libinterceptor-x86_64.so /usr/local/bin/libinterceptor-haswell
 rm build-wrapper-linux-x86.zip
 
 #Download and install SonarScanner
-wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.4.0.2170-linux.zip
-unzip sonar-scanner-cli-4.4.0.2170-linux.zip
-rm sonar-scanner-cli-4.4.0.2170-linux.zip
-mv sonar-scanner-4.4.0.2170-linux /usr/lib/sonar-scanner
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-8.0.1.6346-linux-x64.zip
+unzip sonar-scanner-cli-8.0.1.6346-linux-x64.zip
+rm sonar-scanner-cli-8.0.1.6346-linux-x64.zip
+mv sonar-scanner-8.0.1.6346-linux-x64 /usr/lib/sonar-scanner
 ln -s /usr/lib/sonar-scanner/bin/sonar-scanner /usr/local/bin/sonar-scanner
 
 #export SONAR_RUNNER_HOME=/usr/lib/sonar-scanner
